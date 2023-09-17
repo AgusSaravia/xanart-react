b4084d5fc9cdbb15ed5a4d5247268aa1869de5c6
AgusSaravia <agustinsaravia268@gmail.com>
2023-09-16 21:03:24 -0300

    Added utility classes

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,11 +1,12 @@
 import Image from "next/image"
-import { hero_text, hero_title } from "../data"
+import { firstN, surname, hero_text } from "../data"
+import ScrollingDiv from "./Scroll"
 
-const HeroText=  ({paragraph, title, anchor_text}) =>{
+const HeroText=  ({paragraph, firstN, anchor_text, surname}) =>{
 return(
     <>
-    <h1 className="heroTitle">{title}</h1>
-    <p className="heroParagraph">{paragraph}</p>
+    <h1 className="heroTitle">{firstN}<span className="surname">{surname}</span></h1>
+    <p className="heroParagraph w-[40svw]">{paragraph}</p>
     <div className="heroAnchorContainer">
     <a href="#" className="heroAnchor">{anchor_text}</a>
     </div>
@@ -15,15 +16,21 @@ return(
 
 
 
+
+
 const Hero = () => {
+
+  
+
   return (
-        <section className="bg-[url('/aletatuando.png')] bg-no-repeat bg-cover w-[100svw] min-h-screen items-start px-5 bg-center  ">
-
-            <div className=" px-[10rem] py-[10rem] ">
-              <HeroText paragraph={hero_text} title={hero_title} anchor_text={"GaLEriA"}/>
+        <section className="bg-[url('/aletatuando.png')] bg-no-repeat bg-cover w-[100svw] min-h-full items-start px-5 bg-center">
+            <div   className=" flex-col flex px-[1.5rem] lg:px-[10rem] py-[10rem] font-eczar sm:relative sm:bottom-[10rem] ">
+              <HeroText paragraph={hero_text} firstN={firstN} surname={surname} anchor_text={"Mis trabajos"}/>
         
+            
+              <div  className="self-center mt-[10rem] animate-bounce "><ScrollingDiv text={'scroll'}/></div>
+          
             </div>
-
 
         </section>    
 
