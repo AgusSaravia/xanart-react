69be3f56b63365fd508992db8f9bfd703ddc4e9d
AgusSaravia <agustinsaravia268@gmail.com>
2023-09-19 16:33:22 -0300

    Minor fix

--- app/page.js
+++ app/page.js
@@ -43,6 +43,7 @@ export default function Home() {
     <main className="flex min-h-screen  flex-col items-center justify-between ">
       <Header/>
       <Hero/>
+      <Card cardText={"lorem lorem lorem lorem"}/>
       <Form />
     </main>
   )
