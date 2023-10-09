47700015d4816bc7ff6c961aa313fc8c33afaa67
AgusSaravia <agustinsaravia268@gmail.com>
2023-10-09 14:57:33 -0300

    Added Contact Form with emailjs integration

--- app/data.js
+++ app/data.js
@@ -4,25 +4,32 @@ export const navigation = [
   { name: 'About', link: '#' },
   { name: 'Contact', link: '#' }
 ]
+const public_key = 'KyrlWZqMzet3THDnm'
+const service_ID  = 'test_xanart'
+const template_ID = 'template_s43ghy9'
 
 let hero_text = "Me especializo en X, desde el 20XX comence a tatuar persigo X y me gusta hacer X"
 const firstN = "Alexander"
 const surname = "Rodriguez"
 
 
-const artist = [
+const artists = [
   {
   name: firstN,
   surname: surname,
   links:{
     instagram:"https://www.instagram.com/xanart.tattoo/",
     telegram: " https://t.me/xxanxs"  
-  }
-}]
-console.log(artist)
+  },
+  description: "blablablablablablablablablablablablablablablablablablablabla"
+}
+];
 export {
-  artist as ALEXANDER_DATA,
+  artists ,
   hero_text,
   firstN,
-  surname
+  surname,
+  service_ID,
+  template_ID,
+  public_key
 }
--- app/page.js
+++ app/page.js
@@ -1,39 +1,9 @@
 'use client'
+
 import Hero from "./components/Hero"
 import Header from "./components/Header"
-
-import CallToAction from "./components/Cta"
-
-const INPUT_CLASSES = "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-
-function FormInput({ title, id, inputProps }) {
-  return (
-    <div className="w-full">
-      <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor={id}>
-        {title}
-      </label>
-      <div className="mt-2">
-        <input className={INPUT_CLASSES} name={inputProps}  />
-      </div>
-    </div>
-  )
-}
-function Form() {
-  return (
-    <section className="w-full max-w-lg">
-      <h2 className="text-base font-semibold leading-7 text-gray-900">Contactame!</h2>
-      <form action="#" method="post">
-        <FormInput title="coso" id="jaja" />
-        <input type="text" name="Name" id="name" placeholder="Nombre" />
-        <input type="text" name="Surname" id="surname" placeholder="Apellido" />
-        <input type="email" name="Email" id="email" placeholder="Email" />
-        <input placeholder="Telefono" type="tel" id="phone" name="Phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{3} " />
-        <button type="submit" id="form-submit">GO!</button>
-      </form>
-    </section>
-  )
-}
-
+import Form from "./Contact-Us/page"
+import CallToAction from "./components/Cta.js"
 
 
 
