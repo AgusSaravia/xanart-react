34f8463af40c94bf1b0154b93ccd1fa66a30381b
AgusSaravia <agustinsaravia268@gmail.com>
2024-05-19 16:33:22 -0300

    Fix on Hero class

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -36,14 +36,14 @@ const HeroGrid = () => {
   return (
     <div className="flex-1 flex items-center p-6">
       <div className=" h-full w-full flex flex-col items-center sm:flex-row justify-center mt-6 px-6 gap-6 max-w-3xl lg:max-w-6xl mx-auto ">
-        <div className="sm:self-start sm:basis-1/3 ">
+        <div className="sm:self-start sm:h-auto sm:basis-1/3 ">
           <HeroText textSize={"text-4xl text-center uppercase lg:text-5xl "}>
             Vos pones
             <br />
             la piel
           </HeroText>
         </div>
-        <div className="basis-[50%] max-h-[50%] sm:basis-1/3 aspect-[1215/1715] bg-[url('/logo.svg')] " />
+        <div className="basis-[50%]  sm:basis-1/3 aspect-[1215/1715] bg-[url('/logo.svg')] " />
         <div className="sm:self-end sm:basis-1/3">
           <HeroText textSize={"text-4xl text-center lg:text-5xl uppercase"}>
             Nosotros
